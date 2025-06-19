a1a5bbadd928170043a7a02c8e285a1c135e4c9f
Nguyen020905 <izneo666@gmail.com>
2025-06-19 01:17:16 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const REDIRECT_URL_AFTER_LOGIN = "https://spotify-app-sandy-phi.vercel.app/";
 const SCOPES = ["user-top-read"];
 
-import React from "react";
-
 const App = () => {
   const handLogin = () => {
     window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES}&response_type=token&show_diaglog=true`;
