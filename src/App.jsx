e0eadd36b6ddd12edac8181966d02d130f02ef78
Nguyen020905 <izneo666@gmail.com>
2025-06-19 01:57:25 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { generateCodeVerifier, generateCodeChallenge } from "./pkce";
 
 const CLIENT_ID = "f310e379912b4a998967c82421ea67f4";
 const REDIRECT_URI = "https://spotify-app-sandy-phi.vercel.app/";
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
 const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
 const SCOPES = ["user-top-read"];
+// src/pkce.js
+
+const generateRandomString = (length) => {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const values = crypto.getRandomValues(new Uint8Array(length));
+  return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+};
+
+const codeVerifier = generateRandomString(64);
+
+const sha256 = async (plain) => {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  return window.crypto.subtle.digest("SHA-256", data);
+};
+
+const base64encode = (input) => {
+  return btoa(String.fromCharCode(...new Uint8Array(input)))
+    .replace(/=/g, "")
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
+};
+
+export const generateCodeChallenge = async (verifier) => {
+  const hashed = await sha256(codeVerifier);
+  const codeChallenge = base64encode(hashed);
+};
 
 const App = () => {
   const [accessToken, setAccessToken] = useState(null);
