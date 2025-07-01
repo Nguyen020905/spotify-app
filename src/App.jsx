ea5fd99ef941d276052cec8f5bee5e880aed9c4e
Nguyen020905 <izneo666@gmail.com>
2025-06-30 23:08:29 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import useSpotifyAuth from "./useSpotifyAuth";
 import ArtistList from "./ArtistList";
 import "animate.css";
 
+const [timeRange, setTimeRange] = useState("short-term");
+
 const App = () => {
   const { accessToken, handleLogin, handleLogout } = useSpotifyAuth();
   const [topArtists, setTopArtists] = useState([]);
@@ -14,7 +16,7 @@ const App = () => {
     const fetchTopArtists = async () => {
       try {
         const response = await fetch(
-          "https://api.spotify.com/v1/me/top/artists?limit=10",
+          "https://api.spotify.com/v1/me/top/artists?limit=10&time_range=${timeRange}",
           {
             headers: {
               Authorization: `Bearer ${accessToken}`,
@@ -45,6 +47,16 @@ const App = () => {
           <button className="btn" onClick={handleLogout}>
             Logout
           </button>
+          /*drop down for the time range */
+          <select
+            className="btn"
+            value={timeRange}
+            onChange={(e) => setTimeRange(e.target.value)}
+          >
+            <option value="short_term">Last 4 Weeks</option>
+            <option value="medium_term">Last 6 Months</option>
+            <option value="long_term">All Time</option>
+          </select>
           <h2>Your Top 10 Artists:</h2>
           <ArtistList artists={topArtists} />
         </>
