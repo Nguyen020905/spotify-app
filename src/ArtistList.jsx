3742a7ea661c735e23c9eb0a709ca841842d1a95
Nguyen020905 <izneo666@gmail.com>
2025-06-30 22:07:53 -0700

    Update ArtistList.jsx

--- src/ArtistList.jsx
+++ src/ArtistList.jsx
@@ -5,8 +5,12 @@ const ArtistList = ({ artists }) => {
   return (
     <ul className="list_artist">
       {artists.length > 0 ? (
-        artists.map((artist) => (
-          <li key={artist.id} className="artist_item">
+        artists.map((artist, index) => (
+          <li
+            key={artist.id}
+            className="artist_item animate__animated animate__fadeInUp"
+            style={{ animationDelay: `${index * 0.1}s` }} // staggered delay
+          >
             <img
               src={artist.images[0]?.url}
               alt={artist.name}
